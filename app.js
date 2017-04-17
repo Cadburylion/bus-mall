952e59b67fa8f48b577511bca3d2ad82a77084e9
Matthew Parker <cadburylion@outlook.com>
2017-04-17 14:31:43 -0700

    added two more user stories

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var randomImagesArray = [
+var randomImages = [
   'img/bag.jpg',
   'img/banana.jpg',
   'img/bathroom.jpg',
@@ -21,3 +21,42 @@ var randomImagesArray = [
   'img/usb.gif',
   'img/water-can.jpg',
   'img/wine-glass.jpg'];
+
+function Image(name, filepath, htmlId) {
+  this.name = name;
+  this.filepath = filepath;
+  this.shownAmount = 0;
+  this.clickAmount = 0;
+  this.htmlId = htmlId;
+}
+
+function handleImageDisplayClick(event){
+  // stop the browser from reloading
+  event.preventDefault();
+
+  var picture = event.target; // event target is always the object you added the event listener to
+
+
+  var randomImage = new Image(name, filePath, htmlId);
+  // image.getCookiesPerHour();
+  // image.getTable();
+
+}
+
+var displayRandomImage = document.getElementById('display-new-image');
+displayRandomImage.addEventListener('click', handleImageDisplayClick);
+// var bathroom = new Image('bathroom', 'img/bathroom.jpg', 'test');
+
+
+
+
+//   function openPack() {
+//     var playerArraycopy = randomImages.slice();//getting new copy of initial input
+//     function getRandomImage(){
+//         return playerArraycopy.splice([Math.floor(Math.random() * playerArraycopy.length)],1)[0];//calculating a new random image and removing it from copy to exclude duplicates
+//     }
+//
+//    document.getElementById("player2").src = "image/" + getRandomImg();
+//    document.getElementById("pack").src = "image/" + getRandomImg();
+//    document.getElementById("player3").src = "image/" + getRandomImg();
+// }
--- user_stories.md
+++ user_stories.md
@@ -7,12 +7,16 @@ As a market researcher, I want an app that's focuses the focus group participant
 
 As a market researcher, I want an app that's easy to use and update.
 
+As a market researcher, I want an app that clearly shows products to the participant so they can make their choices without distraction.
+
 
 As a developer, I want to use object-oriented programming to keep the code easy to read.
 
 As a developer, I want to make a dynamic site that can adapt to the marketing team's needs.
 
 As a developer, I want to style the app to appear professional and appeal to a wide variety of users.
+
+As a developer, I want to observe proper semantics so my code is easier to read.
 
 
 As a user, I want an app that displays items relevant to my interests.
