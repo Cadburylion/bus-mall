407619af35451efdfb3a17e09838be22c260acf0
Matthew Parker <cadburylion@outlook.com>
2017-04-17 13:42:36 -0700

    added array with all image filepath names in javascript

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 'use strict';
+
+var randomImagesArray = [
+  'img/bag.jpg',
+  'img/banana.jpg',
+  'img/bathroom.jpg',
+  'img/boots.jpg',
+  'img/breakfast',
+  'img/bubblegum.jpg',
+  'img/chair',
+  'img/cthulhu.jpg',
+  'img/dog-duck.jpg',
+  'img/dragon.jpg',
+  'img/pen.jpg',
+  'img/pet-sweetp.jpg',
+  'img/scissors.jpg',
+  'img/shark.jpg',
+  'img/sweep.png',
+  'img/tauntaun.jpg',
+  'img/unicorn.jpg',
+  'img/usb.gif',
+  'img/water-can.jpg',
+  'img/wine-glass.jpg'];
